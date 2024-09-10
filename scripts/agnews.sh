d1c88eefe51684db630ea3b3f042abe962032599
dfan <fan1dy97@gmail.com>
2024-09-10 14:37:42 +0000

    add run scripts

---
+++ scripts/agnews.sh
@@ -0,0 +1,14 @@
+python ./src/main.py --config_format lora --use_pretrained gpt2 --seed 42 --no_compile \
+--lora_rank 8 --eval_freq 10 --pretraining_rounds 0 --iterations 200 \
+--lora_mlp --lora_causal_self_attention --lora_freeze_all_non_lora \
+--trust dynamic-ref --dataset agnews --num_clients 4 --wandb --wandb_project agnews-nicolas
+
+python ./src/main.py --config_format lora --use_pretrained gpt2 --seed 45 --no_compile \
+--lora_rank 8 --eval_freq 10 --pretraining_rounds 0 --iterations 200 \
+--lora_mlp --lora_causal_self_attention --lora_freeze_all_non_lora \
+--trust dynamic-ref --dataset agnews --num_clients 4 --wandb --wandb_project agnews-nicolas
+
+python ./src/main.py --config_format lora --use_pretrained gpt2 --seed 47 --no_compile \
+--lora_rank 8 --eval_freq 10 --pretraining_rounds 0 --iterations 200 \
+--lora_mlp --lora_causal_self_attention --lora_freeze_all_non_lora \
+--trust dynamic-ref --dataset agnews --num_clients 4 --wandb --wandb_project agnews-nicolas
